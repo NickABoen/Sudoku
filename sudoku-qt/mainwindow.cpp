2bc03d42f2ed3148afc0a02aa1d02104c9ba0019
Ryan Troxler <rtroxler@ksu.edu>
2013-10-07 23:03:39 -0500

    Added very basic GUI functionality prototype.

---
+++ sudoku-qt/mainwindow.cpp
@@ -0,0 +1,19 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::slot11changed(QString text)
+{
+    ui->label->setText(text);
+}
