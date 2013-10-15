e9764dbe976ca47ae4a26de45e0c188cc940a8a6
Ryan Troxler <rtroxler@ksu.edu>
2013-10-15 09:40:01 -0500

    Set up an input mask for the fields
    Input mask only accepts values 1-9.
    
    Also increased size of fields, need to work on the gui design because it
    looks terrible at the moment.

--- sudoku-qt/mainwindow.cpp
+++ sudoku-qt/mainwindow.cpp
@@ -13,11 +13,12 @@ MainWindow::MainWindow(QWidget *parent) :
 
             //Creating and formatting each QLineEdit field.
             fields[i][j] = new QLineEdit(" ",this);
+            fields[i][j]->setInputMask("D");
             fields[i][j]->setObjectName(QString::number(i) + "_" + QString::number(j));
-            fields[i][j]->setFixedHeight(31);
-            fields[i][j]->setFixedWidth(41);
+            fields[i][j]->setFixedHeight(51);
+            fields[i][j]->setFixedWidth(61);
             fields[i][j]->setAlignment(Qt::AlignCenter);
-            fields[i][j]->setStyleSheet("font: 18pt;");
+            fields[i][j]->setStyleSheet("font: 28pt;");
 
             connect(fields[i][j], SIGNAL(textChanged(QString)),this,SLOT(fieldChanged(QString)));
         }
@@ -32,17 +33,17 @@ MainWindow::~MainWindow()
 
 void MainWindow::fieldChanged(QString text)
 {
+    QLineEdit *field = (QLineEdit *)sender();
     //Will eventually parse the object name '1_1' (row 1, col 1) into a position array,
     //and pass the position and values to the Controller, for now just update label to display Value/Field.
-    QLineEdit *field = (QLineEdit *)sender();
     ui->label->setText("Value: " +text + " Field: " + field->objectName());
 }
 
 
 void MainWindow::createLayout()
 {
-    ui->gridLayout->setMargin(6);
-    ui->gridLayout->setSpacing(6);
+    ui->gridLayout->setMargin(0);
+    ui->gridLayout->setSpacing(0);
 
     for(int i = 1; i < 10; i++) {
         for(int j = 1; j < 10; j++) {
