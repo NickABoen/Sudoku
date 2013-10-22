c893508895d019f78a0ee2f955c1991cb894065f
Logan Barker <logbark@k-state.edu>
2013-10-21 21:58:56 -0500

    Added empty classes and wired up buttons to controller

--- sudoku-qt/main.cpp
+++ sudoku-qt/main.cpp
@@ -1,11 +1,13 @@
-#include "mainwindow.h"
 #include <QApplication>
+
+#include "MainController.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+//    MainWindow w;
+//    w.show();
+    Controller::MainController controller;
 
     return a.exec();
 }
