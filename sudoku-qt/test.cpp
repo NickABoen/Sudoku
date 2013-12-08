e45be6b3f11833b2d3e790539a05af32b61f96ba
Logan Barker <logbark@k-state.edu>
2013-12-08 10:01:14 -0600

    Add test, comments, library for image processing, and fix bugs

---
+++ sudoku-qt/test.cpp
@@ -0,0 +1,4 @@
+#include "test.h"
+
+bool test = true;
+std::ofstream testfile("test.txt", std::ofstream::out);
---
+++ sudoku-qt/test.h
@@ -0,0 +1,9 @@
+#ifndef TEST_H
+#define TEST_H
+
+#include <fstream>
+
+extern bool test;
+extern std::ofstream testfile;
+
+#endif // TEST_H
